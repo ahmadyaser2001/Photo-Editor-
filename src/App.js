d268a16298cfcfce8958e1ed326590ffe92dee0c
Ahmad Yaser <ahmadyaserazizi2001@gmail.com>
2022-05-14 09:44:08 +0430

    movie

--- src/App.js
+++ src/App.js
@@ -1,124 +1,58 @@
-import React, { useState } from "react";
+import React,{useEffect,useState} from 'react'  
+import MovieCard from './MovieCard';
 import './App.css';
-import Slider from "./Slider.js";
-import SidebarItem from './SidebarItem'
+import search from './search.svg'
+// dad36ab0
 
-const DEFAULT_OPTION = [
-  {
-    name:'Brightness',
-    property:'brightness',
-    value:100,
-    range:{
-      min:0,
-      max:200,
-    },
-    unit:'%'
-  },
-  {
-    name:'Contrast',
-    property:'contrast',
-    value:100,
-    range:{
-      min:0,
-      max:200,
-    },
-    unit:'%'
-  },
-  {
-    name:'Saturation',
-    property:'saturate',
-    value:100,
-    range:{
-      min:0,
-      max:200,
-    },
-    unit:'%'
-  },
+const APT_URL = 'https://www.omdbapi.com?apikey=dad36ab0'; 
 
-  {
-    name:'Grayscale',
-    property:'Grayscale',
-    value:0,
-    range:{
-      min:0,
-      max:100,
-    },
-    unit:'%'
-  },
-  {
-    name:'Sepia',
-    property:'sepia',
-    value:0,
-    range:{
-      min:0,
-      max:100,
-    },
-    unit:'%'
-  },
-  {
-    name:'Hue Rotate',
-    property:'hue-rotate',
-    value:0,
-    range:{
-      min:0,
-      max:360,
-    },
-    unit:'deg'
-  },
-  {
-    name:'Blur',
-    property:'blur',
-    value:0,
-    range:{
-      min:0,
-      max:20,
-    },
-    unit:'px'
-  },]
-function App() {
-  const [selectedOptionIndex, setSelectedOptionIndex] = useState(0);
-  const [options, setOptions] =useState(DEFAULT_OPTION)
-  const setlectedOptions = options[selectedOptionIndex]
 
-  function handSliderChange({target}){
-    setOptions(prevOptions =>{
-      return prevOptions.map((option,index)=>{
-        if(index !== selectedOptionIndex) return option
-        return{...option,value :target.value}
-      })
-    })
-  }
 
-  function getImageStyle() {
-     const filters = options.map(option=>{
-       return `${option.property}(${option.value}${option.unit})`
-     })
-     return{filter:filters.join(' ')}
-  }
+const App = () => {
+  const [movies ,setMovies] =useState([]);
+  const [searchTerm ,setSearchTerm] = useState('');
+    const searchMovies = async (title)=>{
+        const response = await fetch(`${APT_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search)
+    }
+    useEffect(() => {
+        return () => {
+            searchMovies('Spiderman');
+        };
+    }, [])
   return (
-    <div className="App">
-       <div className="contaner">
-         <div className="main-image" style={getImageStyle()}/>
-         <div className="sildebar">
-          {options.map((option, index)=>{
-            return (
-              <SidebarItem key={index} name={option.name}
-              active={index === selectedOptionIndex}
-              handelClick={()=>setSelectedOptionIndex(index)} />
-            )
-          })}
-
+    <div className='app'>
+         <h1>MovieLand</h1>
+         <div className='search'>
+            <input
+             placeholder='Search for movies'
+             value={searchTerm}
+             onChange={(e)=>setSearchTerm(e.target.value)}
+            />
+            <img src={search}
+            alt="search"
+            onClick={()=>searchMovies(searchTerm)}/>
          </div>
-         <Slider
-         min={setlectedOptions.range.min}
-         max={setlectedOptions.range.max}
-         value={setlectedOptions.value}
-         handChange={handSliderChange}
-         />
-       </div>
+
+         {
+          movies?.length>0
+          ?(
+            <div className='container'>
+            {movies.map((movie)=>(
+              <MovieCard movie={movie}/>
+            ))}
+         </div>
+          ):
+          (
+            <div className='empty'>
+              <h2>No movies found</h2>
+            </div>
+          )
+         }
        
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
